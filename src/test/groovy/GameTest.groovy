f07bf6b5fea3d22a847bbcea6a4a45739c9e19c8
Abraham Fathman <pcg@fathman.com>
2014-07-09 03:54:25 -0400

    Updating timelog

--- src/test/groovy/GameTest.groovy
+++ src/test/groovy/GameTest.groovy
@@ -12,4 +12,14 @@ class GameTest {
     def game = new Game([pair, flush])
     assert flush == game.winner
   }
+  
+  /*
+  * Move the hand comparison into the Game class making it implement the Comparator interface.
+  * Keep track of the "winning reason"
+  * 
+  * We will also need high card to keep track of "which rank" won the hand.
+  * This sets the trend that "maybe it is ok for compareTo to change the state of the object(s)?
+  * If we are ok with compareTo chaging the state them PokerHand could keep the compareTo method....
+  * 
+  */
 }
